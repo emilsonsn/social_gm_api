982bd4314dd74e62b8fb96a8c9141045c7cb6e81
emilsonsn <emilsonsn2@gmail.com>
2024-12-22 15:51:06 -0300

    feat: implementa criação de usuários iniciais

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Enums\UserRoleEnum;
+use App\Models\Instance;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -13,32 +14,65 @@ class UserSeed extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        
-        User::firstOrCreate([
-            'email' => 'admin@admin',
-        ],
-        [
-            'name' => 'Admin',
-            'password' => Hash::make('admin'),
-            'phone' => '83991236636',
-            'cpf_cnpj' => '13754674412',
-            'birth_date' => '2001-12-18',
-            'is_active' => true,
-            'role' => UserRoleEnum::Admin->value,
-        ]);
 
-        User::firstOrCreate([
-            'email' => 'user@user',
-        ],
-        [
-            'name' => 'User',
-            'password' => Hash::make('user'),
-            'phone' => '83991236636',
-            'cpf_cnpj' => '13754674413',
-            'birth_date' => '2001-12-18',
-            'is_active' => true,
-            'role' => UserRoleEnum::User->value,
-        ]);
+    {
+        $users = [
+            [
+                'email' => 'admin@admin',
+                'name' => 'Admin',
+                'password' => Hash::make('admin'),
+                'phone' => '83991236636',
+                'cpf_cnpj' => '13754674412',
+                'birth_date' => '2001-12-18',
+                'is_active' => true,
+                'role' => UserRoleEnum::Admin->value,
+            ],
+            [
+                'email' =>  'gm@guimadureira.com',
+                'name' => 'Guilherme',
+                'password' => Hash::make('@sfmG2024'),
+                'phone' => '',
+                'cpf_cnpj' => '13754674411',
+                'birth_date' => '2001-12-18',
+                'is_active' => true,
+                'role' => UserRoleEnum::User->value,
+            ],
+            [
+                'email' => 'ph@guimadureira.com',
+                'name' => 'Pedro',
+                'password' => Hash::make('Polinho2019'),
+                'phone' => '',
+                'cpf_cnpj' => '13754674414',
+                'birth_date' => '2001-12-18',
+                'is_active' => true,
+                'role' => UserRoleEnum::User->value,
+            ]
+        ];
+
+        $instances = [
+            [
+                'instance' => 'guimadureira',
+                'email'    => 'gm@guimadureira.com'
+            ],
+            [
+                'instance' => 'pedroh',
+                'email' => 'ph@guimadureira.com',
+            ]
+        ];
+
+        foreach ($users as $user) {
+            $userCreated = User::updateOrCreate([
+                'email' => $user['email'],
+            ], $user);
+
+            $userInstance = collect($instances)->firstWhere('email', $user['email']);
+
+            if ($userInstance) {
+                Instance::where('name', $userInstance['instance'])
+                    ->update([
+                        'user_id' => $userCreated->id,
+                    ]);
+            }
+        } 
     }
 }
